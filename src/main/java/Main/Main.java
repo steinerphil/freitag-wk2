042a37f2601a3d38fdd1adb7c0918383d47b3ed1
Philipp Steiner <philipp-steiner@gmx.de>
2021-09-19 22:43:55 +0200

    final project code

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -10,15 +10,15 @@ public class Main {
         //create shopService
         ShopService shop1 = new ShopService();
 
-        shop1.listProducts();
-        shop1.getProduct(10);
-        shop1.addOrder(new int[]{10});
-        shop1.addOrder(new int[]{40, 20, 30, 10});
-        shop1.addOrder(new int[]{20, 20,});
-        shop1.addOrder(new int[]{40, 10, 30});
-        System.out.println("*************");
-        shop1.listOrders();
-        shop1.getOrder(2);
+//        shop1.listProducts();
+//        shop1.getProduct(10);
+//        shop1.addOrder(new int[]{10});
+//        shop1.addOrder(new int[]{40, 20, 30, 10});
+//        shop1.addOrder(new int[]{20, 20,});
+//        shop1.addOrder(new int[]{40, 10, 30});
+//        System.out.println("*************");
+//        shop1.listOrders();
+//        shop1.getOrder(2);
 
     }
 
